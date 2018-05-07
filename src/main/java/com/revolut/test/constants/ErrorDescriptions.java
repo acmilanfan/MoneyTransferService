9e2f51fa22e0a0a4f89ce7bdad14e9e2cf52989d
acmilanfan <gentoo1993@gmail.com>
2018-05-07 09:23:41 +0300

    Fix error description

--- src/main/java/com/revolut/test/constants/ErrorDescriptions.java
+++ src/main/java/com/revolut/test/constants/ErrorDescriptions.java
@@ -7,5 +7,5 @@ public class ErrorDescriptions {
     public static final String RETRIEVING_DATA_ERROR_DESCRIPTION = "Error while retrieving data from the request";
     public static final String TRANSFERRING_MONEY_ERROR_DESCRIPTION = "Error while transferring money";
     public static final String NOT_ENOUGH_MONEY_ERROR_DESCRIPTION = "Not enough money for the transfer";
-    public static final String INCORRECT_REQUEST_DATA_ERROR_DESCRIPTION = "Not enough money for the transfer";
+    public static final String INCORRECT_REQUEST_DATA_ERROR_DESCRIPTION = "Incorrect request data";
 }
